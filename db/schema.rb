770ed543308bc0b5895fa1aef20ffe90f4b8b709
apopma <adam.popma@gmail.com>
2015-09-16 11:30:34 -0700

    use arbitrary-precision floats instead

--- db/schema.rb
+++ db/schema.rb
@@ -17,16 +17,16 @@ ActiveRecord::Schema.define(version: 20150916175651) do
   enable_extension "plpgsql"
 
   create_table "houses", force: :cascade do |t|
-    t.text     "street",                               null: false
-    t.string   "status",                               null: false
-    t.integer  "price",                                null: false
-    t.integer  "bedrooms",                             null: false
-    t.integer  "bathrooms",                            null: false
-    t.integer  "sq_ft",                                null: false
-    t.decimal  "lat",        precision: 16, scale: 15
-    t.decimal  "lng",        precision: 17, scale: 16
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.text     "street",     null: false
+    t.string   "status",     null: false
+    t.integer  "price",      null: false
+    t.integer  "bedrooms",   null: false
+    t.integer  "bathrooms",  null: false
+    t.integer  "sq_ft",      null: false
+    t.float    "lat",        null: false
+    t.float    "lng",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
