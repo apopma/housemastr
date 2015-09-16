162c89495a68473dfab607b1069271ee0020b20d
apopma <adam.popma@gmail.com>
2015-09-16 11:58:01 -0700

    add a bunch of database validations

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,7 +1,14 @@
 class House < ActiveRecord::Base
   validates :street, :status, :price, :bedrooms, :bathrooms, :sq_ft,
             :latitude, :longitude, presence: true
-  validates_inclusion_of :status, in: %w( active pending sold )
-  validates_inclusion_of :latitude, in: (-90..90)
-  validates_inclusion_of :longitude, in: (-180..180)
+
+  validates_inclusion_of :status, in: %w( active pending sold ),
+    message: "has to be 'active', 'pending', or 'sold'"
+
+  validates_inclusion_of :latitude, in: (-90..90), message: "is not on this planet"
+  validates_inclusion_of :longitude, in: (-180..180), message: "is not on this planet"
+
+  validates_uniqueness_of :street, scope: [
+    :status, :price, :bedrooms, :bathrooms, :latitude, :longitude
+  ], message: "is a duplicate of some other database record"
 end
